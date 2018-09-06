ca426b4196137af969ea9bc61d6c3c2b3786dd60
coder <mshawnlouw@gmail.com>
2018-09-06 12:26:02 +0200

    Just added a flash message and no numbers should be taken on the text input

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ let greetings = require('./greetings');
 let app = express();
 
 app.use(session({
-    secret: 'keyboard cat5 run all 0v3r',
+    secret: 'This is going to be a line that I will use to display an error',
     resave: false,
     saveUninitialized: true
 }));
+app.use(flash());
 //----------FOR SENDING DATA AS A FORM TO THE SERVER!!! -------
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,7 +32,7 @@ if (process.env.DATABASE_URL) {
   useSSL = true;
 }
 
-const connectionString = process.env.DATABASE_URL || 
+const connectionString = process.env.DATABASE_URL ||
 "postgresql://coder:coder123@localhost:5432/greetings_webapp";
 
 const pool = new Pool({
@@ -40,7 +41,6 @@ const pool = new Pool({
 });
 
 let greetingsInstance = greetings(pool);
-
 
 app.engine('handlebars', expressHandlebars({defaultLayout: 'links'}));
 app.set('view engine', 'handlebars');
@@ -56,12 +56,12 @@ app.post('/greetings', async function(req, res){
 // -----GO TO HTML ----
     let text  = req.body.textInput;
 		let radio = req.body.language;
-    //
-    // if(text === "" || text === undefined){
-    //   req.flash("info", "Please enter a name")
-    // } else if(radio ===undefined){
-    //   req.flash("info", "Please select a language")
-    // }
+
+    if(text == "" || text == undefined){
+      req.flash("info", "Please enter your name!");
+    } else if(radio == undefined || !radio){
+      req.flash("info", "Please select a language!");
+    }
 
 		var results = {
 			greeting: await greetingsInstance.greet(text, radio),
@@ -71,7 +71,6 @@ app.post('/greetings', async function(req, res){
     res.render('greet', results);
 });
 
-
 app.get('/greetings/database', async function(req,res){
 let users = await greetingsInstance.returnUsers();
 		res.render('db', {users});
@@ -80,7 +79,7 @@ let users = await greetingsInstance.returnUsers();
 app.get('/reset', async function(req, res){
 	 await greetingsInstance.reset()
 
-		res.redirect('/');
+		res.render('db');
 });
 
 
