44711d10fbaef2a0f2c7c6c724f24c5d46994974
coder <mshawnlouw@gmail.com>
2018-09-07 14:34:36 +0200

    Adding a database on travis

--- tables.sql
+++ tables.sql
@@ -3,6 +3,6 @@ drop table if exists users;
 create table users (
   id serial not null primary key,
   name varchar(50) not null,
-  counter int not null default 0,
+  counter int not null default 0
   -- language varchar(50) not null
 );
